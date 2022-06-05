73d3103cbd432aa5f9f0dffd1270d29e1ba0f80c
chenshenhai <cshenhai@gmail.com>
2022-06-05 22:49:25 +0800

    feat: add switch tab style

--- packages/playground/src/modules/view-switch.vue
+++ packages/playground/src/modules/view-switch.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="iexample-view-switch">
     <div class="view-switch-header">
-      switch
+      <div>Left</div>
+      <div class="view-switch-tablist">
+        <div class="view-switch-tab">
+          Markdown
+        </div>
+        <div class="view-switch-tab active">
+          Demo
+        </div>
+      </div>
+      <div>Right</div>
     </div>
     <div class="view-switch-content">
       <code-view></code-view>
@@ -21,15 +30,49 @@ import CodeView from './code-view.vue';
   flex-direction: column;
 
   .view-switch-header {
-    height: 40px;
-    display: flex;
+    height: 36px;
+    box-sizing: border-box;
+    padding: 0 20px;
     border-bottom: 1px solid var(--iexample-border-color);
     box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
 
   .view-switch-content {
     display: flex;
     flex: 1;
   }
+
+  .view-switch-tablist {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid var(--iexample-border-color-active);
+    border-radius: 14px;
+    overflow: hidden;
+  }
+
+  .view-switch-tab {
+    display: flex;
+    height: 28px;
+    padding: 0 20px;
+    min-width: 120px;
+    box-sizing: border-box;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    border-right: 1px solid var(--iexample-border-color-active);
+    cursor: pointer;
+
+    &.active {
+      background: var(--iexample-bg-active);
+    }
+
+    &:last-child {
+      border-right: none;
+    }
+  }
+
 }
 </style>
